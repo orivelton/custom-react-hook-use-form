9378e5c4879e8b111a422ed47712516b7f4ad28b
Orivelton <orivelton@Oriveltons-MBP.Home>
2020-04-13 12:36:40 +0100

    chore: added custom hook

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,18 +1,30 @@
 import React from 'react';
+import useForm from '../utils/State/useForm';
 
 const Form = () => {
-  const handleSubmit = e => {
-    e.preventDefault();
-    console.log('Enter!');
+  
+  const enter = () => {
+    console.log('Values!', values);
   }
+  const [values,  handleChange, handleSubmit] = useForm(enter);
 
   return (
     <form onSubmit={handleSubmit}>
       <label>Name:</label>
-      <input type="text" />
+      <input 
+        type="text"
+        name="name"
+        value={values.name || ''}
+        onChange={handleChange}
+      />
 
       <label>Password:</label>
-      <input type="text" />
+      <input
+        type="password"
+        name="password"
+        value={values.password || '' }
+        onChange={handleChange}
+      />
 
       <button>Enter -></button>
     </form>
--- src/utils/State/useForm.js
+++ src/utils/State/useForm.js
@@ -1,7 +1,19 @@
 import  { useState } from 'react';
 
-const useForm = () => {
+const useForm = event => {
   const [state, setState] = useState({});
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    event();
+  }
+
+  const handleChange = e => {
+    e.persist();
+    setState(state => ({...state, [e.target.name]: e.target.value}))
+  }
+
+  return [state, handleChange, handleSubmit];
 
 };
 
